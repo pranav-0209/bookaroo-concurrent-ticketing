fbe0cbbf9374a47a719026ccf5cf5ea8746354d0
Pranav <pnarvekar0209@gmail.com>
2026-01-05 12:34:00 +0530

    Initial Spring Boot project setup

---
+++ bookaroo-backend/src/main/java/com/pranav/bookaroo_backend/BookarooBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pranav.bookaroo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookarooBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookarooBackendApplication.class, args);
+	}
+
+}
---
+++ bookaroo-backend/src/test/java/com/pranav/bookaroo_backend/BookarooBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranav.bookaroo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookarooBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
