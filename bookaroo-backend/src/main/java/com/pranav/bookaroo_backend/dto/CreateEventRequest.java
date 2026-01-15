3183048a4fc8dd8d8e20144e9ddc47f6a265f7d9
Pranav <pnarvekar0209@gmail.com>
2026-01-15 11:37:25 +0530

    CreateEventRequest DTO class created

--- bookaroo-backend/src/main/java/com/pranav/bookaroo_backend/dto/CreateEventRequest.java
+++ bookaroo-backend/src/main/java/com/pranav/bookaroo_backend/dto/CreateEventRequest.java
@@ -1,23 +1,27 @@
 package com.pranav.bookaroo_backend.dto;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class BookingRequest {
+public class CreateEventRequest {
 
-    @NotNull
-    private Long eventId;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String venue;
     @Min(1)
-    private int quantity;
-    @Email
-    private String userEmail;
+    private int totalTickets;
+    @NotNull
+    private LocalDateTime eventDateTime;
 }
